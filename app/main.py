7caa43ba2309bf6eb65d5250afd32748699fdb15
Hyeongju Lee <zoocasso1590@gmail.com>
2023-08-22 17:01:39 +0900

    update

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("./")
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
---
+++ config_sample.py
@@ -0,0 +1,7 @@
+#config.py는 보안상 git에 안올리며, 구조는 아래와 같음
+DATABASE_CONFIG = {
+    'host':'127.0.0.1',
+    'user':'scott',
+    'password':'tiger',
+    'dbname':'dbname'
+}
